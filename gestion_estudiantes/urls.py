761f4543adba6e96e303d5fa6893ac230be46f76
danilomartinezcanon15-af <danilomartinezcanon15@gmail.com>
2026-02-19 16:47:34 -0500

    Bienvenido y registro

--- gestion_estudiantes/urls.py
+++ gestion_estudiantes/urls.py
@@ -4,11 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.bienvenido, name='bienvenido'),
     path('registro/', views.registro_usuario, name='registro'),
-    path('login/', views.login, name='login'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('logout/', views.cerrar_sesion, name='logout'),
-    path('estudiantes/', views.listar_estudiantes, name='listar_estudiantes'),
-    path('estudiantes/añadir/', views.anadir_estudiante, name='anadir_estudiante'),
-    path('estudiantes/eliminar/<str:estudiante_id>/', views.eliminar_estudiante, name='eliminar_estudiante'),
-    path('estudiantes/editar/<str:estudiante_id>/', views.editar_estudiante, name='editar_estudiante')
 ]
