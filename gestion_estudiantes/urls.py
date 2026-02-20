123eef855ed4d6f47e739d5958deee2fb62eb93d
danilomartinezcanon15-af <danilomartinezcanon15@gmail.com>
2026-02-19 19:55:46 -0500

    Path de dashboard y logout

--- gestion_estudiantes/urls.py
+++ gestion_estudiantes/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.bienvenido, name='bienvenido'),
     path('registro/', views.registro_usuario, name='registro'),
     path('login/', views.login, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.cerrar_sesion, name='logout'),
 ]
