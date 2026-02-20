51171fd6a17c26f09486cfd72f03b1f45179186a
danilomartinezcanon15-af <danilomartinezcanon15@gmail.com>
2026-02-19 19:47:56 -0500

    Prueba

--- gestion_estudiantes/urls.py
+++ gestion_estudiantes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.bienvenido, name='bienvenido'),
     path('registro/', views.registro_usuario, name='registro'),
+    path('login/', views.login, name='login'),
 ]
